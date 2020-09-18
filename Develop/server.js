e45cdf55634b0150881059b61f73d566cbed0cca
Miller Rich <william.miller.rich@gmail.com>
2020-09-17 19:00:51 -0500

    app.get index.html and notes.html, set up watch script in package.json

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,13 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.listen(PORT, function() {
     console.log("app listening at http://localhost:" + PORT);
